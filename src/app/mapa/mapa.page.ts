b3d635831e4d9dde97b3b108dfedcd53cdf3261e
Barbara722 <46595432+Barbara722@users.noreply.github.com>
2019-11-11 10:47:12 -0300

    mapa

---
+++ src/app/mapa/mapa.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.page.html',
+  styleUrls: ['./mapa.page.scss'],
+})
+export class MapaPage implements OnInit {
+  map: mapboxgl.Map;
+
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = -22.8994473;
+  lng = -43.5589886;
+
+  constructor() {
+    (mapboxgl as typeof mapboxgl).accessToken =  environment.mapbox.accessToken;
+  }
+
+  ngOnInit() {
+    
+    
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 13,
+      center: [this.lng, this.lat]
+  });
+
+
+  }
+ 
+
+}
