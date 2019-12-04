b26a468a263a61ecddd198593a327e3641c3ce6c
LeoFodon <leonardofs2@gmail.com>
2019-12-04 11:42:59 -0300

    location mapbox
    daniel fez tudo

--- src/app/location/location.page.ts
+++ src/app/location/location.page.ts
@@ -56,6 +56,8 @@ export class LocationPage implements OnInit {
         
         response.forEach(doc=>{ 
         
+
+          this.firestorage.storage.ref().child(`perfil/${doc.payload.doc.id}.jpg`).getDownloadURL().then(url =>{
           let c = new Perfil();
           //@ts-ignore
           c.setPerfil(doc.payload.doc.data(),doc.payload.doc.id);
@@ -73,94 +75,107 @@ export class LocationPage implements OnInit {
                 "sobrenome": c.sobrenome,
                 "email": c.email,
                 "id": doc.payload.doc.id,
+                "image" : url
     
             }
         });
 
+      });
+
+
+      this.loadMapa(resp.coords.longitude,resp.coords.latitude);
         },err=>{ 
           console.log(err);
         })
   
       });
 
-      //Insere as informações do perfil[] nas features do mapa
-    var stores = {
-      "type": "FeatureCollection",
-      "features": this.perfil
-    };  
-   //cria o mapa e centraliza nas coordendas do usuario
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: this.style,
-      zoom: 13,
-      center: [resp.coords.longitude, resp.coords.latitude],
-      
-      //espera o mapa para carregar as lojas ou marcações
-  }).on('load',function(resp){
-
-
-    //Define imagem que sera usada como marcador
-      resp.target.loadImage('../../assets/gpsPink.png', 
-      function(error, image) {
-
-        resp.target.addImage('marcador', image);
-
-        //@ts-ignore
-        resp.target.addLayer({
-          id: 'locations',
-          type: 'symbol',
-          // Add a GeoJSON source containing place coordinates and information.
-          source: {
-            type: "geojson",
-            
-            data: stores
-          },
-          layout: {
-            "icon-image": "marcador",
-            "icon-size": 0.05
-          }
-        })
-  }
-  
-  )
-  });
-
-  //ao clickar no marcador pega as informações do firebase inseridas no mapa
-  this.map.on('click', 'locations', function (dadosMapa) {
-
-    this.nome = dadosMapa.features[0].properties.nome;
-    this.servico = dadosMapa.features[0].properties.servicos;  
-    this.sobrenome = dadosMapa.features[0].properties.sobrenome;  
-    this.email = dadosMapa.features[0].properties.email;  
-
-    document.getElementById("nome").innerHTML = this.nome+" "+this.sobrenome;
-    document.getElementById("email").innerHTML = this.email;
-    document.getElementById("servicos").innerHTML = this.servico;
-    
-    
-
-    let ref = this.firestorage.storage.ref().child(`perfil/${dadosMapa.features[0].properties.id}.jpg`);
-    ref.getDownloadURL().then(url =>{
-    this.imagem = url;
-    
-    });
-    
-  });
-
-  
-
-  //Botão para localizar usuario precisamente
-  this.map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions: {
-    enableHighAccuracy: true
-    },
-    trackUserLocation: true
-    }));
+ 
 
   }).catch((error) => {
     console.log('Error getting location', error);
   });
 
+  }
+
+
+  loadMapa(longitude,latitude){
+         //Insere as informações do perfil[] nas features do mapa
+         var stores = {
+          "type": "FeatureCollection",
+          "features": this.perfil
+        };  
+       //cria o mapa e centraliza nas coordendas do usuario
+        this.map = new mapboxgl.Map({
+          container: 'map',
+          style: this.style,
+          zoom: 13,
+          center: [longitude, latitude],
+          
+          //espera o mapa para carregar as lojas ou marcações
+      }).on('load',function(resp){
+    
+    
+          //Define imagem que sera usada como marcador
+            resp.target.loadImage('../../assets/gpsPink.png', 
+            function(error, image) {
+    
+              resp.target.addImage('marcador', image);
+    
+              //@ts-ignore
+              resp.target.addLayer({
+                id: 'locations',
+                type: 'symbol',
+                // Add a GeoJSON source containing place coordinates and information.
+                source: {
+                  type: "geojson",
+                  
+                  data: stores
+                },
+                layout: {
+                  "icon-image": "marcador",
+                  "icon-size": 0.05
+                }
+              })
+        }
+        
+        )
+    
+    
+    
+      });
+    
+      
+      
+      //ao clickar no marcador pega as informações do firebase inseridas no mapa
+      this.map.on('click', 'locations', function (dadosMapa) {
+    
+        this.nome = dadosMapa.features[0].properties.nome;
+        this.servico = dadosMapa.features[0].properties.servicos;  
+        this.sobrenome = dadosMapa.features[0].properties.sobrenome;  
+        this.email = dadosMapa.features[0].properties.email;  
+    
+        document.getElementById("nome").innerHTML = this.nome+" "+this.sobrenome;
+        document.getElementById("email").innerHTML = this.email;
+        document.getElementById("servicos").innerHTML = this.servico;
+        
+        console.log(dadosMapa.features[0].properties.image);
+        document.getElementById("myImg").src = dadosMapa.features[0].properties.image;
+        
+    
+        
+        
+      });
+    
+      
+    
+      //Botão para localizar usuario precisamente
+      this.map.addControl(new mapboxgl.GeolocateControl({
+        positionOptions: {
+        enableHighAccuracy: true
+        },
+        trackUserLocation: true
+        }));
   }
 
   
